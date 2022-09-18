66475f58f4288002e61bec94f96cf2f0504e7b15
Alex McBrier <58454148+alexmcbrier@users.noreply.github.com>
2022-09-17 20:43:29 -0400

    Update preferences.php

--- preferences.php
+++ preferences.php
@@ -1,80 +1,20 @@
 
 <?php
 session_start();
-if (isset($_SESSION["user_id"])) {
-   
-    $mysqli = require __DIR__ . "/config.php";
-   
-    $sql = "SELECT * FROM user
-            WHERE id = {$_SESSION["user_id"]}";
-           
-    $result = $mysqli->query($sql);
-   
-    $user = $result->fetch_assoc();
-    $id = ($user["id"]);
-   
-    //for some reason the id is not working for the WHERE
-}
-function changeFontSize($size)
+function changePreference($type, $value)
 {
-  //if signed in
-  if (isset($_SESSION["user_id"])) {
-    //neccessary to setup
-    $mysqli = require __DIR__ . "/config.php";
-   
-    $sql = "SELECT * FROM user
-            WHERE id = {$_SESSION["user_id"]}";
-           
-    $result = $mysqli->query($sql);
-   
-    $user = $result->fetch_assoc();
- 
-    //change
-    $id = ($user["id"]);
-    $link = require __DIR__ . "/config.php";
-    if($link === false){
-        die("ERROR: Could not connect. " . mysqli_connect_error());
- 
-    }
-    $sql = "UPDATE user SET fontSize=$size WHERE id=$id";
-    if(mysqli_query($link, $sql)){
-        echo "Record was updated successfully.";
-    } else {
-        echo "ERROR: Could not able to execute $sql. "
-                                . mysqli_error($link);
- 
-    }
-    mysqli_close($link);
-    header("location: index.php");
-  }
-  else
-  {
-    header("location: index.php");
- 
-  }
-}
-function changeTheme($theme)
-{
-   //if signed in
    if (isset($_SESSION["user_id"])) {
-    //neccessary to setup
     $mysqli = require __DIR__ . "/config.php";
- 
     $sql = "SELECT * FROM user
             WHERE id = {$_SESSION["user_id"]}";
-           
     $result = $mysqli->query($sql);
-   
     $user = $result->fetch_assoc();
- 
-    //theme changing
     $id = ($user["id"]);
     $link = require __DIR__ . "/config.php";
     if($link === false){
         die("ERROR: Could not connect. " . mysqli_connect_error());
- 
     }
-    $sql = "UPDATE user SET theme='$theme' WHERE id=$id";
+    $sql = "UPDATE user SET $type='$value' WHERE id=$id";
     if(mysqli_query($link, $sql)){
         echo "Record was updated successfully.";
     } else {
@@ -82,7 +22,6 @@ function changeTheme($theme)
                                 . mysqli_error($link);
  
     }
- 
     mysqli_close($link);
     header("location: index.php");
   }
@@ -92,66 +31,10 @@ function changeTheme($theme)
  
   }
 }
-  if (isset($_GET['size1'])) {
-    $size = 1;
-    changeFontSize($size);
-  }
-  if (isset($_GET['size2'])) {
-    $size = 1.5;
-    changeFontSize($size);
-  }
-  if (isset($_GET['size3'])) {
-    $size = 2;
-    changeFontSize($size);
-  }
-  if (isset($_GET['size4'])) {
-    $size = 2.5;
-    changeFontSize($size);
-  }
-  if (isset($_GET['size5'])) {
-    $size = 3;
-    changeFontSize($size);
-  }
-  if (isset($_GET['lexendDeca'])) {
-    $font = 3;
-    changeFontSize($size);
-  }
-  //themes
-  if (isset($_GET['theme-olivia'])) {
-    $theme = "olivia";
-    changeTheme($theme);
-  }
-  if (isset($_GET['theme-dracula'])) {
-    $theme = "dracula";
-    changeTheme($theme);
-  }
-  if (isset($_GET['theme-8008'])) {
-    $theme = "8008";
-    changeTheme($theme);
-  }
-  if (isset($_GET['theme-mizu'])) {
-    $theme = "mizu";
-    changeTheme($theme);
-  }
-  if (isset($_GET['theme-striker'])) {
-    $theme = "striker";
-    changeTheme($theme);
-  }
-  if (isset($_GET['theme-blueberry'])) {
-    $theme = "blueberry";
-    changeTheme($theme);
-  }
-  if (isset($_GET['theme-creamsicle'])) {
-    $theme = "creamsicle";
-    changeTheme($theme);
-  }
-  if (isset($_GET['theme-botanical'])) {
-    $theme = "botanical";
-    changeTheme($theme);
-  }
-  if (isset($_GET['theme-luna'])) {
-    $theme = "luna";
-    changeTheme($theme);
+  if (isset($_GET['change'])) {
+    $type = htmlspecialchars($_GET["type"]);
+    $value = htmlspecialchars($_GET["value"]);
+    changePreference($type, $value);
   }
 ?>
 <!DOCTYPE html>
@@ -176,33 +59,53 @@ function changeTheme($theme)
             <li><a href="learn.php">learn</a></li>
     </nav>
         <form id="preferencesArea" method="POST" action="preferences.php">
-            <h1 class = "notSignedIn" id="preferenceHeader">size</h1>
-            <div class ="rowContainer">
-                <a class = "preferencesRow" href="preferences.php?size1=true">1</a>
-                <a class = "preferencesRow" href="preferences.php?size2=true">1.5</a>
-                <a class = "preferencesRow" href="preferences.php?size3=true">2</a>
-                <a class = "preferencesRow" href="preferences.php?size4=true">2.5</a>
-                <a class = "preferencesRow" href="preferences.php?size5=true">3</a>
+          <div class = "horizontalAlign">
+            <div class ="rowContainer" style="width: 40%">
+              <h1 class = "notSignedIn" id="preferenceHeader">size</h1>
+              <a class = "preferencesRow" href="preferences.php?change=true&type=fontSize&value=1">1</a>
+              <a class = "preferencesRow" href="preferences.php?change=true&type=fontSize&value=2">2</a>
+              <a class = "preferencesRow" href="preferences.php?change=true&type=fontSize&value=3">3</a>
+              <a class = "preferencesRow" href="preferences.php?change=true&type=fontSize&value=4">4</a>
+              <a class = "preferencesRow" href="preferences.php?change=true&type=fontSize&value=5">5</a>
             </div>
-            <h1 class = "notSignedIn" id="preferenceHeader">font</h1>
-            <div class ="rowContainer">
-                <a class = "preferencesRow" href="preferences.php?size1=true">arial</a>
-                <a class = "preferencesRow" href="preferences.php?size2=true">helvetica</a>
-                <a class = "preferencesRow" href="preferences.php?size3=true">futura</a>
-                <a class = "preferencesRow" href="preferences.php?size4=true">lexendDeca</a>
-                <a class = "preferencesRow" href="preferences.php?size5=true">verdana</a>
+            <div class ="rowContainer" style="width: 60%">
+              <h1 class = "notSignedIn" id="preferenceHeader">font</h1>
+                <a class = "preferencesRow" href="preferences.php?change=true&type=fontSize&value=1">arial</a>
+                <a class = "preferencesRow" href="preferences.php?change=true&type=fontSize&value=1">helvetica</a>
+                <a class = "preferencesRow" href="preferences.php?change=true&type=fontSize&value=1">futura</a>
+                <a class = "preferencesRow" href="preferences.php?change=true&type=fontSize&value=1">lexendDeca</a>
+                <a class = "preferencesRow" href="preferences.php?change=true&type=fontSize&value=1">verdana</a>
             </div>
+          </div>
+          <div class = "horizontalAlign">
+            <div class ="rowContainer" style="width: 40%">
+              <h1 class = "notSignedIn" id="preferenceHeader">caret</h1>
+              <a class = "preferencesRow" href="preferences.php?change=true&type=caret&value=none">none</a>
+              <a class = "preferencesRow" href="preferences.php?change=true&type=caret&value=underline">underline</a>
+              <a class = "preferencesRow" href="preferences.php?change=true&type=caret&value=highlight">highlight</a>
+            </div>
+            <div class ="rowContainer" style="width: 35%">
+              <h1 class = "notSignedIn" id="preferenceHeader"># of lines</h1>
+                <a class = "preferencesRow" href="preferences.php?change=true&type=lineCount&value=1">1</a>
+                <a class = "preferencesRow" href="preferences.php?change=true&type=lineCount&value=2">2</a>
+                <a class = "preferencesRow" href="preferences.php?change=true&type=lineCount&value=3">3</a>
+                <a class = "preferencesRow" href="preferences.php?change=true&type=lineCount&value=4">4</a>
+            </div>
+            <div class ="rowContainer" style="width: 25%">
+              <h1 class = "notSignedIn" id="preferenceHeader">blur text</h1>
+                <a class = "preferencesRow" href="preferences.php?change=true&type=textBlur&value=on">on</a>
+                <a class = "preferencesRow" href="preferences.php?change=true&type=textBlur&value=off">off</a>
+            </div>
+          </div>
+            <div class ="rowContainer">
             <h1 id="preferenceHeader">theme</h1>
-            <div class ="rowContainer">
-                <a class = "themesRow" id = "theme-olivia" href="preferences.php?theme-olivia=true">olivia</a>
-                <a class = "themesRow" id = "theme-dracula" href="preferences.php?theme-dracula=true">dracula</a>
-                <a class = "themesRow" id = "theme-8008" href="preferences.php?theme-8008=true">8008</a>
-                <a class = "themesRow" id = "theme-mizu" href="preferences.php?theme-mizu=true">mizu</a>
-                <a class = "themesRow" id = "theme-striker" href="preferences.php?theme-striker=true">striker</a>
-                <a class = "themesRow" id = "theme-blueberry" href="preferences.php?theme-blueberry=true">blueberry</a>
-                <a class = "themesRow" id = "theme-creamsicle" href="preferences.php?theme-creamsicle=true">creamsicle</a>
-                <a class = "themesRow" id = "theme-botanical" href="preferences.php?theme-botanical=true">botanical</a>
-                <a class = "themesRow" id = "theme-luna" href="preferences.php?theme-luna=true">luna</a>
+                <a class = "themesRow" id = "theme-olivia" href="preferences.php?change=true&type=theme&value=olivia">olivia</a>
+                <a class = "themesRow" id = "theme-dracula" href="preferences.php?change=true&type=theme&value=dracula">dracula</a>
+                <a class = "themesRow" id = "theme-8008" href="preferences.php?change=true&type=theme&value=8008">8008</a>
+                <a class = "themesRow" id = "theme-mizu" href="preferences.php?change=true&type=theme&value=mizu">mizu</a>
+                <a class = "themesRow" id = "theme-striker" href="preferences.php?change=true&type=theme&value=striker">striker</a>
+                <a class = "themesRow" id = "theme-blueberry" href="preferences.php?change=true&type=theme&value=blueberry">blueberry</a>
+                <a class = "themesRow" id = "theme-creamsicle" href="preferences.php?change=true&type=theme&value=creamsicle">creamsicle</a>
             </div>
             
 </form>
