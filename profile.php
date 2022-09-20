0b4cd6e93596d4473c2572b04b7ac85e175a0115
Alex McBrier <58454148+alexmcbrier@users.noreply.github.com>
2022-09-20 14:08:37 -0400

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,15 +1,5 @@
 <?php
 session_start();
-if(isset($_COOKIE["rememberMe"]))
-{
-    $mysqli = require __DIR__ . "/config.php";
-    $name = $_COOKIE["rememberMe"];
-    //change to whatever
-    $sql = "SELECT id FROM user WHERE email = '$name'";
-    $result = $mysqli->query($sql);
-    $user = $result->fetch_assoc();
-    $_SESSION["user_id"] = $user["id"];
-}
 function getInfo($id)
 {
     $mysqli = require __DIR__ . "/config.php";
