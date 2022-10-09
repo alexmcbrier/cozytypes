69e47444ede1f435cfda1dc56fd986963aa9e95a
Alex McBrier <58454148+alexmcbrier@users.noreply.github.com>
2022-10-09 11:59:40 -0400

    Update learn.php

--- learn.php
+++ learn.php
@@ -1,4 +1,19 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=TAG_ID"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)};
+          gtag('js', new Date());
+          gtag('config', 'G-9W2ZHHJ7P5');
+        </script>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JMV592"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        
+    </head>
     <h1>in progress...</h1>
 </html>
