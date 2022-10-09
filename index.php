ba69cd097b7cea59776281b8700b1e86f0f09d06
Alex McBrier <58454148+alexmcbrier@users.noreply.github.com>
2022-10-09 11:53:16 -0400

    Update index.php

--- index.php
+++ index.php
@@ -24,13 +24,15 @@ if (isset($_GET["finish"]))
 <!DOCTYPE html>
 <html>
     <head>
-                <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5JMV592');</script>
-        <!-- End Google Tag Manager -->
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=TAG_ID"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+
+  gtag('config', 'G-9W2ZHHJ7P5');
+</script>
         
             <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JMV592"
