86081762d599f9122d61a7c5c366e795b79562fc
Alex McBrier <58454148+alexmcbrier@users.noreply.github.com>
2022-10-09 11:43:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -24,6 +24,14 @@ if (isset($_GET["finish"]))
 <!DOCTYPE html>
 <html>
     <head>
+                <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5JMV592');</script>
+        <!-- End Google Tag Manager -->
+        
         <link rel="shortcut icon" type="image/x-icon" href="images\keyboard.ico" />
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -35,6 +43,10 @@ if (isset($_GET["finish"]))
         <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
     </head>
     <body>
+            <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JMV592"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <nav>
             <li>CozyTypes</li>
             <li><a id = "play" href="index.php">play</a></li>
