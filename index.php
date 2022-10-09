e298f2224495b2b7a2721918768fec2377b43ae2
Alex McBrier <58454148+alexmcbrier@users.noreply.github.com>
2022-10-09 11:48:43 -0400

    Update index.php

--- index.php
+++ index.php
@@ -32,6 +32,11 @@ if (isset($_GET["finish"]))
         })(window,document,'script','dataLayer','GTM-5JMV592');</script>
         <!-- End Google Tag Manager -->
         
+            <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JMV592"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+        
         <link rel="shortcut icon" type="image/x-icon" href="images\keyboard.ico" />
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
