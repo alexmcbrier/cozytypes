b0585c1753f4e332f8a706f54658391cfce52221
Alex McBrier <58454148+alexmcbrier@users.noreply.github.com>
2022-10-09 11:58:53 -0400

    Update testFinished.php

--- testFinished.php
+++ testFinished.php
@@ -28,6 +28,19 @@ if (isset($_SESSION["user_id"])) {
 <!DOCTYPE html>
 <html>
     <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=TAG_ID"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)};
+          gtag('js', new Date());
+          gtag('config', 'G-9W2ZHHJ7P5');
+        </script>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JMV592"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        
         <link rel="shortcut icon" type="image/x-icon" href="images\keyboard.ico" />
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
