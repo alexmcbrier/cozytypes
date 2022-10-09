cb75ae1091f9821fd215fa3b4f0d8024d27a9da0
Alex McBrier <58454148+alexmcbrier@users.noreply.github.com>
2022-10-09 11:23:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -24,13 +24,14 @@ if (isset($_GET["finish"]))
 <!DOCTYPE html>
 <html>
     <head>
-        <!-- Google Tag Manager -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-5JMV592');</script>
-        <!-- End Google Tag Manager -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9W2ZHHJ7P5"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-9W2ZHHJ7P5');
+        </script>
 
         <link rel="shortcut icon" type="image/x-icon" href="images\keyboard.ico" />
         <meta charset="UTF-8">
@@ -43,10 +44,6 @@ if (isset($_GET["finish"]))
         <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
     </head>
     <body>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JMV592"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
     <nav>
             <li>CozyTypes</li>
             <li><a id = "play" href="index.php">play</a></li>
