487abafef327f094702adf83c2dd04688ee43cb3
Alex McBrier <58454148+alexmcbrier@users.noreply.github.com>
2022-10-29 22:11:55 -0400

    Update style.php

--- style.php
+++ style.php
@@ -63,7 +63,7 @@ if ($caret == "none")
 }
 else if ($caret == "underline")
 {
-    $caretTop = $fontSize * 1.45;
+    $caretTop = $fontSize * 1.35;
     $caretHeight = $fontSize / 3;
 }
 else if ($caret == "highlight")
