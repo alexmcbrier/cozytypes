e1464373cf4a1ebc8cc0b2e71c401306372d9d90
Alex McBrier <58454148+alexmcbrier@users.noreply.github.com>
2022-11-02 09:32:56 -0400

    Update preferences.php

--- preferences.php
+++ preferences.php
@@ -28,7 +28,7 @@
     </head>
     <body>
       <nav>
-            <img width="45" height="45" display = "block" src="images/panda2.png">
+            <img width="55" height="55" display = "block" src="images/panda2.png">
             <li style="padding-left: .5rem; padding-right: 2rem">CozyTypes</li>
             <li ><a id = "play" href="index.php"><img width="45" height="45" display = "block" src="images/keyboard.png"></a></li>
             <li><a href="login.php"><img width="35" height="35" display = "block" src="images/person.png"></a></li>
