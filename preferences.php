37bfaae52e0e00daa39036db2af30ec8c4cafbf7
alexmcbrier <alexmcbrier@icloud.com>
2022-11-07 11:31:05 -0500

    fix preferences

--- preferences.php
+++ preferences.php
@@ -97,12 +97,38 @@
         </div>
     </form>
     <script>
-            findCookie('fontSize', 'sizesContainer').classList.add("currentSetting");
-            findCookie('fontFamily', 'fontsContainer').classList.add("currentSetting");
-            findCookie('caret', 'caretsContainer').classList.add("currentSetting");
-            findCookie('lineCount', 'linesContainer').classList.add("currentSetting");
-            findCookie('blur', 'blurContainer').classList.add("currentSetting");
-            findCookie('theme', 'themesContainer').innerHTML  += " ✦";
+        function getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i <ca.length; i++) {
+                let c = ca[i];
+                while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+                }
+            }
+            return "";
+        }
+        function findCookie(name, parentId) //checks to see which cookie is selected (used in prefrences to highlight current setting)
+        {
+            const children = document.getElementById(parentId).children // wpm Display
+            for (var i = 0; i < children.length; i++) {
+                var child = children[i];
+                if (getCookie(name) == child.title)
+                {
+                return child;
+                }
+            }
+        }
+        findCookie('fontSize', 'sizesContainer').classList.add("currentSetting");
+        findCookie('fontFamily', 'fontsContainer').classList.add("currentSetting");
+        findCookie('caret', 'caretsContainer').classList.add("currentSetting");
+        findCookie('lineCount', 'linesContainer').classList.add("currentSetting");
+        findCookie('blur', 'blurContainer').classList.add("currentSetting");
+        findCookie('theme', 'themesContainer').innerHTML  += " ✦";
     </script>
 </body>
 
