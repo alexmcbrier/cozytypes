c0d7a2ff07f8b26993fce6954382e3256faa5bb8
Alex <alexmcbrier@icloud.com>
2022-11-12 19:26:00 -0500

    fix

--- head.php
+++ head.php
@@ -19,10 +19,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CozyTypes</title>
-    <link rel="stylesheet" href="style.php">
-    <script defer type="text/javascript" src="script.js">
-        addCookies();
-    </script>
+    <script defer type="text/javascript" src="script.js">addCookies();</script> <!-- must go before so cookies are correct -->
+        <link rel="stylesheet" href="style.php">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Comfortaa&family=Courier+Prime&family=IBM+Plex+Sans&family=Lexend+Deca&family=Lora&family=Merriweather&family=Nunito&family=PT+Serif&family=Raleway&family=Source+Code+Pro&family=Titillium+Web&display=swap" rel="stylesheet">
