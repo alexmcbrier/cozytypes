7e5d156116019eec2af0c2db6f4ac28b0619d8a1
Alex <alexmcbrier@icloud.com>
2022-11-13 17:40:06 -0500

    icons

--- nav.php
+++ nav.php
@@ -3,7 +3,8 @@
         <img alt="logo" width="55" height="55" display="block" src="images/panda2.png">
         <li style="padding-left: .5rem; padding-right: 2rem;">CozyTypes</li>
     </a>
-    <i href="index.php" class="fa-regular fa-user"></i>
+
+    <a href="/profile"><i class="fa-regular fa-user"></i></a>
     <li><a href="login.php"><img title="Profile" width="35" height="35" display="block" src="images/person.png"></a></li>
     <li><a href="preferences.php"><img title="Settings" width="35" height="35" display="block" src="images/setting.png"></a></li>
 </nav>
