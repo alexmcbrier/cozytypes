107dd1314df3349d74589ce4750a457038447d82
Alex <alexmcbrier@icloud.com>
2022-11-12 19:29:51 -0500

    fix

--- head.php
+++ head.php
@@ -20,7 +20,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CozyTypes</title>
     <script defer type="text/javascript" src="script.js"></script>
-    <script type="text/javascript">
+    <script type="text/javascript" src="script.js">
         addCookies();
     </script>
     <link rel="stylesheet" href="style.php">
