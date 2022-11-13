9903220111ccee74e3840427f27e217cf091214e
Alex <alexmcbrier@icloud.com>
2022-11-12 19:09:19 -0500

    test

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const displayTimer = document.getElementById('time') // Time Display
 const displayWPM = document.getElementById('wpmDisplay') // wpm Display
 const mainContent = document.getElementById('mainContent') // wpm Display
 var timerVar = "not running" // Turns on when user begins typing...
-var sentenceLength = 50;
+var sentenceLength = 2;
 var time = 15;
 var wordList = ["the", "be", "to", "of", "and", "a", "in", "that", "have", "I", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if", "about", "who", "get", "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", "know", "take", "people", "into", "year", "your", "good", "some", "could", "them", "see", "other", "than", "then", "now", "look", "only", "come", "its", "over", "think", "also", "back", "after", "use", "two", "how", "our", "work", "first", "well", "way", "even", "new", "want", "because", "any", "these", "give", "day", "most", "us"]
 if (checkCookie("words")) {
@@ -393,7 +393,7 @@ function findCookie(name, parentId) //checks to see which cookie is selected (us
                 }
             }
         }
-        
+
 function updatePreferences()
 {
     if(checkCookie('fontSize'))
