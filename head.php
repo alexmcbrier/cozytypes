f04f0d17b49b3d8d4ee54a57223f25b844d8db8b
Alex <alexmcbrier@icloud.com>
2022-11-12 19:31:52 -0500

    f

--- head.php
+++ head.php
@@ -20,7 +20,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CozyTypes</title>
     <script defer type="text/javascript" src="script.js"></script>
-    <script type="text/javascript" src="script.js">
     <link rel="stylesheet" href="style.php">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
