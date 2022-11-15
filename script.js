c64b84502bf111481fecd0a56a69668aa0543eb7
Alex <alexmcbrier@icloud.com>
2022-11-14 22:00:47 -0500

    fix time

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const displayWPM = document.getElementById('wpmDisplay') // wpm Display
 const mainContent = document.getElementById('mainContent') // wpm Display
 var timerVar = "not running" // Turns on when user begins typing...
 var sentenceLength = 50;
-var time = 15;
+var time = 0;
 var wordList = ["the", "be", "to", "of", "and", "a", "in", "that", "have", "I", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if", "about", "who", "get", "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", "know", "take", "people", "into", "year", "your", "good", "some", "could", "them", "see", "other", "than", "then", "now", "look", "only", "come", "its", "over", "think", "also", "back", "after", "use", "two", "how", "our", "work", "first", "well", "way", "even", "new", "want", "because", "any", "these", "give", "day", "most", "us"]
 if (checkCookie("words")) {
     sentenceLength = getCookie("words");
