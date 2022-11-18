19dd31ac4b4fe3f97457288cfa6546bd3fc55025
Alex McBrier <58454148+alexmcbrier@users.noreply.github.com>
2022-11-18 08:35:29 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+            <div id="footer">
+                <a class = "footerLinks" href="https://github.com/alexmcbrier/cozytypes">github <i class="fa-solid fa-code fa-sm"></i></a>
+                <div class = "linkDivider">/</div>
+                <a class = "footerLinks" href="/preferences.php">theme <i class="fa-solid fa-palette fa-sm"></i></a>
+                <div class = "linkDivider">/</div>
+                <a class = "footerLinks" href="/preferences.php">font <i class="fa-solid fa-font fa-sm"></i></a>
+            </div>
