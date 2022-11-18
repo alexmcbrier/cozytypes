022f0b855b8ef77249dcb854e0201834545e4ffc
Alex McBrier <58454148+alexmcbrier@users.noreply.github.com>
2022-11-18 08:27:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -65,13 +65,6 @@ if (isset($_COOKIE["email"])) {
                 <img id="restartTest" onclick="restart()" src="images/refresh-button.png">
             </div>
             </div>
-            <div id="footer">
-                <a class = "footerLinks" href="https://github.com/alexmcbrier/cozytypes">github <i class="fa-solid fa-code fa-sm"></i></a>
-                <div class = "linkDivider">/</div>
-                <a class = "footerLinks" href="/preferences.php">theme <i class="fa-solid fa-palette fa-sm"></i></a>
-                <div class = "linkDivider">/</div>
-                <a class = "footerLinks" href="/preferences.php">font <i class="fa-solid fa-font fa-sm"></i></a>
-            </div>
         <?php } else if (isset($_GET["finish"])) { ?>
             <!-- Display if test IS complete -->
             <div id="middle">
