74cc9eb504d850e65642b849fbebc564f7074dd0
Alex <alexmcbrier@icloud.com>
2022-11-19 16:29:27 -0500

    could not fix

--- nav.php
+++ nav.php
@@ -1,15 +1,3 @@
-<?php
-//grabbing user session information (neccesary for staying signed in etc.)
-if (isset($_COOKIE["email"])) {
-    $mysqli = require __DIR__ . "/config.php";
-    $name = $_COOKIE["email"];
-    //change to whatever
-    $sql = "SELECT username FROM user WHERE email = '$name'";
-    $result = $mysqli->query($sql);
-    $user = $result->fetch_assoc();
-    $displayUsername= $user["username"];
-}
-?>
 <nav>
     <a href="index.php" style="text-decoration: none;">
         <img alt="logo" width="55" height="55" display="block" src="images/panda2.png">
@@ -18,5 +6,4 @@ if (isset($_COOKIE["email"])) {
     <a href="/index.php"><i class="fa-solid fa-house"></i></a>
     <a href="/preferences.php"><i class="fa-solid fa-gear"></i></a>
     <a href="/login.php"><i class="fa-regular fa-user"></i></a>
-    <h1 id = "showName"><?php echo $displayUsername; ?></h1>
 </nav>
