c3e3c819e3e50865b09960e16baa581c027a823d
alexmcbrier <alexmcbrier@icloud.com>
2022-11-20 04:05:11 -0500

    fix

--- footer.php
+++ footer.php
@@ -16,11 +16,11 @@ if (isset($_SESSION["user_id"])) {
         <i class="fa-solid fa-code fa-sm"></i>
     </a>
     <div class = "linkDivider">/</div>
-    <a class = "footerLinks tooltip" href="/preferences.php">theme<span class="tooltiptext"><?php echo htmlspecialchars($user["theme"]); ?></span>
+    <a class = "footerLinks tooltip" href="/preferences.php">theme<span class="tooltiptext"><?= $theme ?></span>
         <i class="fa-solid fa-palette fa-sm"></i>
     </a>
     <div class = "linkDivider">/</div>
-    <a class = "footerLinks tooltip" href="/preferences.php">font<span class="tooltiptext"><?php echo htmlspecialchars($user["theme"]); ?></span>
+    <a class = "footerLinks tooltip" href="/preferences.php">font<span class="tooltiptext"><?= $font ?></span>
         <i class="fa-solid fa-font fa-sm"></i>
     </a>
 </div>
