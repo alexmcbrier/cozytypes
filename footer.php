f38bb0352e6f75c32ea86c7bb9e7be673ece3ba3
alexmcbrier <alexmcbrier@icloud.com>
2022-11-20 04:03:58 -0500

    sdf

--- footer.php
+++ footer.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+if (isset($_SESSION["user_id"])) {
+    $mysqli = require __DIR__ . "/config.php";
+    $sql = "SELECT * FROM user WHERE id = {$_SESSION["user_id"]}";
+    $result = $mysqli->query($sql);
+    $user = $result->fetch_assoc();
+    $font = htmlspecialchars($user["fontSize"]);
+    $id = htmlspecialchars($user["id"]);
+    $font = htmlspecialchars($user["fontSize"]);
+    $theme = htmlspecialchars($user["theme"]);
+}
+?>
 <div id="footer">
     <a class = "footerLinks tooltip" href="https://github.com/alexmcbrier/cozytypes">github<span class="tooltiptext">Support Us!</span>
         <i class="fa-solid fa-code fa-sm"></i>
