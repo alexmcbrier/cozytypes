c85cbad8db598f6392a87366b7d5b73068d77f5b
alexmcbrier <alexmcbrier@icloud.com>
2022-11-20 01:10:21 -0500

    fix signup

--- signup.php
+++ signup.php
@@ -8,11 +8,9 @@ if (isset($_SESSION["user_id"])) {
 }
 $is_invalid = false;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $is_invalid = true;
     if (empty($_POST["username"])) { //if name empty
         $is_invalid = true;
     }
-
     else if (empty($_POST["password"])) { //if passowrd empty
         $is_invalid = true;
     }
@@ -24,19 +22,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     else if  (strlen($_POST["password"]) < 8) { //at least 8 characters
         $is_invalid = true;
     }
-
-    else if  (! preg_match("/[a-z]/i", $_POST["password"])) { //at least 1 character
-        $is_invalid = true;
-    }
-
-    else if  (! preg_match("/[0-9]/i", $_POST["password"])) { //at least 1 number
-        $is_invalid = true;
-    }
-
     else if  ($_POST["password"] !== $_POST["confirm"]) { //make sure confirmation matches
         $is_invalid = true;
     }
-    if ($is_invalid = false)
+    if (!$is_invalid)
     {
         //Credentials are accurate, create a password hash (encrypt)
         $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
@@ -71,21 +60,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 <!DOCTYPE html>
 <html>
-
-<?php include "./head.php" ?>
-
-<body class="main-body">
-    <?php include "./nav.php" ?>
-    <form id="mainContent" method ="post" style = "width: 60%;">
-        <h1 id = "loginHeader">sign up</h1>
-        <?php if ($is_invalid) : ?>
-            <div id="invalid">Invalid Credentials</div>
-        <?php endif; ?>
-        <input type="text" autocomplete = "off" placeholder="username" id="username" name = "username">
-        <input type="text" autocomplete = "off" placeholder="email" id="email"  name = "email">
-        <input type="text" autocomplete = "off" placeholder="password" id="password" name = "password">
-        <input type="text" autocomplete = "off" placeholder="confirm password" id="confirm"  name = "confirm">
-        <button class = "loginBtn" id = "loginButton1" type="submit" value="submit" name="register">Create account</button>
-    </form>  
+    <?php include "./head.php" ?>
+    <body class="main-body">
+        <?php include "./nav.php" ?>
+        <form id="mainContent" method ="post" style = "width: 60%;">
+            <h1 id = "loginHeader">sign up</h1>
+            <?php if ($is_invalid) : ?>
+                <div id="invalid">Invalid Credentials</div>
+            <?php endif; ?>
+            <input type="text" autocomplete = "off" placeholder="username" id="username" name = "username">
+            <input type="text" autocomplete = "off" placeholder="email" id="email"  name = "email">
+            <input type="text" autocomplete = "off" placeholder="password" id="password" name = "password">
+            <input type="text" autocomplete = "off" placeholder="confirm password" id="confirm"  name = "confirm">
+            <button class = "loginBtn" id = "loginButton1" type="submit" value="submit" name="register">Create account</button>
+        </form>  
     </body>
 </html>
