cf401b1d0db282a9961316d38e564f4ccf2c39cb
Alex <alexmcbrier@icloud.com>
2022-11-22 00:06:50 -0500

    fix

--- script.js
+++ script.js
@@ -516,6 +516,7 @@ function updateCookies()
 }
 if (window.location.href == ("https://www.cozytypes.com/")) {
     window.replace("https://cozytypes.com/");
+    window.location.href = ("https://cozytypes.com/");
 }
 document.body.onLoad = updateCookies();
 document.body.onresize = function() { zoomwait() };
