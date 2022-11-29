94776b32f30b71b2894928dda5992b76a6f12363
Alex <alexmcbrier@icloud.com>
2022-11-28 21:54:01 -0500

    remove tooltip

--- footer.php
+++ footer.php
@@ -13,15 +13,15 @@ if (isset($_COOKIE["fontFamily"])) {
         <div><kbd>Tab</kbd> to restart</div>
     </div>
     <div id = "footerDiv">
-        <a class = "footerLinks tooltip" href="https://github.com/alexmcbrier/cozytypes">github<span class="tooltiptext">Support Us!</span>
+        <a class = "footerLinks tooltip" href="https://github.com/alexmcbrier/cozytypes">github</span>
             <i class="fa-solid fa-code fa-sm"></i>
         </a>
         <div class = "linkDivider">/</div>
-        <a class = "footerLinks tooltip" href="/preferences.php">theme<span class="tooltiptext"><?= $theme ?></span>
+        <a class = "footerLinks tooltip" href="/preferences.php">theme</span>
             <i class="fa-solid fa-palette fa-sm"></i>
         </a>
         <div class = "linkDivider">/</div>
-        <a class = "footerLinks tooltip" href="/preferences.php">font<span class="tooltiptext"><?= $font ?></span>
+        <a class = "footerLinks tooltip" href="/preferences.php">font</span>
             <i class="fa-solid fa-font fa-sm"></i>
         </a>
     </div>
