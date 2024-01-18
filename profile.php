207044d25306c52f310eef4fc00255268c968303
Alexander McBrier <58454148+alexmcbrier@users.noreply.github.com>
2024-01-18 14:21:33 -0500

    remove friends from profile

--- profile.php
+++ profile.php
@@ -1,31 +1,12 @@
 <?php
 session_start();
-function getInfo($id)
-{
-    $mysqli = require __DIR__ . "/config.php";
-    $sql = "SELECT user, friend FROM friends";
-    $result = $mysqli->query($sql);
-    $rowcount = mysqli_num_rows($result);
-    $array = array();
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while ($row = $result->fetch_assoc()) {
-            if ($row["user"] == $id) {
-                array_push($array, $row["friend"]);
-            }
-        }
-    }
-    return ($array);
-}
 if (isset($_SESSION["user_id"])) {
-
     $mysqli = require __DIR__ . "/config.php";
 
     $sql = "SELECT * FROM user
             WHERE id = {$_SESSION["user_id"]}";
 
     $result = $mysqli->query($sql);
-
     $user = $result->fetch_assoc();
     $font = htmlspecialchars($user["fontSize"]);
     $id = htmlspecialchars($user["id"]);
@@ -33,9 +14,6 @@ if (isset($_SESSION["user_id"])) {
     $theme = htmlspecialchars($user["theme"]);
     $wpmPR = htmlspecialchars($user["wpm"]);
     $totalTests = htmlspecialchars($user["testsTaken"]);
-    //getting friends
-    $friendsArray = getInfo($user["username"]); //all u have to do is input your id 
-
 } else //if not logged in but somehow managed to get to this page (Neccesary)
 {
     header("Location: login");
@@ -83,58 +61,6 @@ if (isset($_SESSION["user_id"])) {
         </div>
         <?php include "./footer.php" ?>
     </div>
-    <div id="friendsListArea">
-        <div id="friendsListHeader">Friends</div>
-        <div id="friendsList"></div>
-        <form style="box-sizing: border-box; width: 100%;" action="addFriend.php" method="post">
-            <h3>Add Friend</h3>
-            <input type="text" name="friendAccount" autocomplete="off" placeholder="username" id="username" name="username">
-        </form>
-        <div id="closeSidebar" onclick="closeSidebar()">Close</div>
-    </div>
-    <script>
-        // Create element:
-        const parent = document.getElementById('friendsList');
-        const array = <?php echo json_encode($friendsArray); ?>;
-        const length = <?= count($friendsArray) ?>;
-        for (let i = 0; i < length; i++) {
-            //user div
-            const div = document.createElement("div");
-            div.classList.add("friendDiv");
-            parent.appendChild(div);
-            //profile pic
-            //name
-            const name = document.createElement("div");
-            name.classList.add("friend");
-            name.innerHTML = '<img width="20" height="20" src="images/user.png">' + array[i];
-            div.appendChild(name);
-            //invite friend
-            const invite = document.createElement("div");
-            invite.classList.add("inviteFriend");
-            invite.innerHTML = "invite";
-            div.appendChild(invite);
-            //remove friend
-
-            const form = document.createElement("form");
-            form.setAttribute("class", "removeFriend");
-            form.setAttribute("action", "removeFriend.php");
-            form.setAttribute("method", "post");
-            form.classList.add("removeFriend");
-            const button = document.createElement("button");
-            button.setAttribute("name", "friendAccount");
-            button.setAttribute("value", array[i]);
-            button.innerHTML = '<img width="30"height="30" type="submit" value="submit"src="images/delete.png">';
-            div.appendChild(form);
-            form.appendChild(button);
-
-            function openSidebar() {
-                document.getElementById('friendsListArea').className = 'sidebarOpen';
-                document.getElementById('sidebar').className = 'hideSidebar';
-                document.getElementById('statsArea').className = 'statsAreaOpen';
-            };
-
-        }
-    </script>
 </body>
 
 </html>
