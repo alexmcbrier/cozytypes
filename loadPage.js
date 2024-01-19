c50d7e141fc204d4b0e8dace469d4f6ea301f118
Alexander McBrier <alexmcbrier@crc-dot1x-nat-10-239-247-139.bu.edu>
2024-01-18 22:44:23 -0500

    S

--- loadPage.js
+++ loadPage.js
@@ -1,3 +1,6 @@
+var script = document.createElement('script');
+script.src = 'script.js';
+document.head.appendChild(script);
 function loadPage(page) {
     // Create a new XMLHttpRequest object
     var xhttp = new XMLHttpRequest();
@@ -7,6 +10,11 @@ function loadPage(page) {
         if (this.readyState === 4 && this.status === 200) {
             // Replace the content of the 'content' div with the loaded page
             document.getElementsByClassName("main-body")[0].innerHTML = this.responseText;
+            // Check if the loaded page is the original page
+            if (page === 'index.html') {
+                // Execute your specific function for the original page
+                restart();
+            }
         }
     };
     
