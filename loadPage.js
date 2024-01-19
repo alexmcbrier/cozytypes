2bd88b001e6d83a22203cf13e7a52370ff5877ec
Alexander McBrier <alexmcbrier@crc-dot1x-nat-10-239-247-139.bu.edu>
2024-01-18 23:12:41 -0500

    d:

--- loadPage.js
+++ loadPage.js
@@ -1,24 +1,33 @@
 function loadPage(page) {
     // Create a new XMLHttpRequest object
     var xhttp = new XMLHttpRequest();
-    
+
     // Define what happens on successful data submission
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             // Replace the content of the 'content' div with the loaded page
             document.getElementsByClassName("main-body")[0].innerHTML = this.responseText;
-                    // Check if the loaded page is the original page
-            if (page === 'original_page.html') {
-            const scriptElement = document.createElement('script');
-            scriptElement.src = 'script.js';
-            document.head.appendChild(scriptElement);
-        }
-        }
 
-        
+            // Check if the loaded page is the original page
+            if (page === 'original_page.html') {
+                const scriptElement = document.createElement('script');
+                scriptElement.src = 'script.js';
+                document.head.appendChild(scriptElement);
+
+                // Call a function defined in script.js
+                scriptElement.onload = function () {
+                    rerunScript();
+                };
+            }
+        }
     };
-    
+
     // Set up and send the request
     xhttp.open("GET", page, true);
     xhttp.send();
+}
+
+// Function defined in script.js
+function rerunScript() {
+    restart();
 }
