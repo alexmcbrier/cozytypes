f6c49373780172e6148c6163ecd391ebf3a53ff3
Alexander McBrier <alexmcbrier@crc-dot1x-nat-10-239-247-139.bu.edu>
2024-01-18 21:15:46 -0500

    trying to fix reload

---
+++ loadPage.js
@@ -0,0 +1,16 @@
+function loadPage(page) {
+    // Create a new XMLHttpRequest object
+    var xhttp = new XMLHttpRequest();
+    
+    // Define what happens on successful data submission
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            // Replace the content of the 'content' div with the loaded page
+            document.getElementById("content").innerHTML = this.responseText;
+        }
+    };
+    
+    // Set up and send the request
+    xhttp.open("GET", page, true);
+    xhttp.send();
+}
--- nav.php
+++ nav.php
@@ -5,12 +5,13 @@ if (isset($_SESSION["user_id"])) {
     $result = $mysqli->query($sql);
     $user = $result->fetch_assoc();
 } ?>
-<nav>
+<script src="loadPage.js"></script>
+<nav> 
     <a id = "logo" href="https://cozytypes.com" style="text-decoration: none;">
         <li>cozytypes</li>
     </a>
     <a class = "navIcon" href="https://cozytypes.com"><i class="fa-solid fa-house"></i></a>
-    <a class = "navIcon" href="/preferences"><i class="fa-solid fa-gear"></i></a>
+    <a class = "navIcon" href="#" onClick = "loadPage('preferences.php')"><i class="fa-solid fa-gear"></i></a>
     <a href="/login" id = "showUsername">
         <i class="fa-regular fa-user"></i>
         <div><?= htmlspecialchars($user["username"]) ?></div>
