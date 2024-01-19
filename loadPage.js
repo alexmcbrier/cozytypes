3324a65fa4c983c1e0111a5bd9ee103b69da3610
Alexander McBrier <alexmcbrier@crc-dot1x-nat-10-239-247-139.bu.edu>
2024-01-18 23:10:07 -0500

    s

--- loadPage.js
+++ loadPage.js
@@ -7,13 +7,14 @@ function loadPage(page) {
         if (this.readyState === 4 && this.status === 200) {
             // Replace the content of the 'content' div with the loaded page
             document.getElementsByClassName("main-body")[0].innerHTML = this.responseText;
-        }
-        // Check if the loaded page is the original page
-        if (page === 'original_page.html') {
+                    // Check if the loaded page is the original page
+            if (page === 'original_page.html') {
             const scriptElement = document.createElement('script');
             scriptElement.src = 'script.js';
             document.head.appendChild(scriptElement);
         }
+        }
+
         
     };
     
