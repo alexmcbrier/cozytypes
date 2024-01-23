3c759792ee3e838b9136b936852ad58eb8442325
Alexander McBrier <alexmcbrier@crc-dot1x-nat-10-239-247-139.bu.edu>
2024-01-22 23:51:41 -0500

    d

--- style.php
+++ style.php
@@ -332,20 +332,18 @@ include "themes.scss"; //file contains all fonts
         font-size: 2rem;
     }
     .rowContainer {
-    user-select: none;
-    color: var(--currentWord);
-    background-color: var(--rowBackground);
-    font-size: 1rem;
-    border-radius: 1.5rem;
-    padding: 1rem;
-    margin-left: .5rem;
-    line-height: 10px;
-    vertical-align: top;
-    text-decoration: none;
-    /* Remove one of the margin properties */
-    margin: 1rem;
-    align-content: center;
-}
+        user-select: none;
+        color: var(--currentWord);
+        background-color: var(--rowBackground);
+        font-size: 1rem;
+        border-radius: 1.5rem;
+        padding: 1rem;
+        line-height: 10px;
+        vertical-align: top;
+        text-decoration: none;
+        margin: 1rem;
+        align-content: center;
+    }
     #showRestart
     {
         color: var(--row);
@@ -1240,9 +1238,7 @@ include "themes.scss"; //file contains all fonts
     min-width: unset;
     cursor: pointer;
 }
-#themesContainer {
-    display: flex;
-}
+
     #themesContainer a:hover,
     .preferences a:hover {
         transform: scale(1.1, 1.1);
