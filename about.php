b4f4ead64a6f09ab1784be64cca6d8c0d95b973c
Alexander McBrier <alexmcbrier@crc-dot1x-nat-10-239-247-139.bu.edu>
2024-01-23 14:34:00 -0500

    about

--- about.php
+++ about.php
@@ -9,7 +9,12 @@
                 <h1 class="notSignedIn" id="preferenceHeader">about us</h1>
             </div>
             <div id="sizesContainer" class="rowContainer">
-                <h1 class="notSignedIn" id="preferenceHeader">imophi is a small game studio started by two friends with a deep passion for game development. We are so excited to bring our first game, CozyTyper, to anyone who might enjoy it. We have refined this game after a great deal of tester feedback, and look forward to continuing that work to make CozyTyper the best it can be. Thanks for taking the time to consider our game, and we hope you love it as much as we do!</h1>
+                <h1 class="notSignedIn" id="preferenceHeader">
+                imophi is a small game studio started by two friends <br> 
+                with a deep passion for game development. We are so excited <br> 
+                to bring our first game, CozyTyper, to anyone who might enjoy it. <br>
+                We have refined this game after a great deal of tester feedback, <br> 
+                and look forward to continuing that work to make CozyTyper the best it can be. Thanks for taking the time to consider our game, and we hope you love it as much as we do!</h1>
             </div>
         </div>
         <?php include "./footer.php" ?>
