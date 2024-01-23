95441a915ae09ce92249b30ec7e4c3afdf80f667
Alexander McBrier <alexmcbrier@crc-dot1x-nat-10-239-247-139.bu.edu>
2024-01-23 14:28:48 -0500

    about

---
+++ about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<?php include "./head2.php" ?>
+<body class="main-body">
+    <div id="mainContent">
+        <?php include "./nav.php" ?>
+        <div id="middle">
+            <div id="displayStats">
+                <div class="statsContainer"> 
+                    <h1 class="notSignedIn" id="preferenceHeader">about us<i class="fa-regular fa-user"></i></h1>
+            </div>
+        </div>
+        <?php include "./footer.php" ?>
+    </div>
+</body>
+
+</html>
