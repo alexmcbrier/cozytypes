391d83dc5133baa7d45244225630e618774a2f24
Alexander McBrier <58454148+alexmcbrier@users.noreply.github.com>
2024-01-25 08:46:53 -0500

    Update about.php

--- about.php
+++ about.php
@@ -12,12 +12,11 @@ session_start();
                 <h1 class="notSignedIn" id="preferenceHeader" style="margin-left: 0" >about us</h1>
                 <h1 class="aboutDescription">
                 cozytypes is a simple typing website created by Alex McBrier,
-                a student at Boston University with a love for typing. 
-                Test your typing abilities in different modes, track your 
+                a student at Boston University, to test your typing abilities in different modes, track your 
                 progress, and improve your overall typing speed. 
-                I am very excited to bring this game to you and anyone else who 
-                may enjoy it, and am looking forward to hearing feedback. I hope you love it as much as I do!<br><br>
-                Contact us via email: <a class = "aboutLink" href="mailto:alexmcbrier@gmail.com">alexmcbrier@gmail.com</a>
+                I am excited to bring this game to you and anyone else who 
+                may enjoy it, and am looking forward to hearing feedback.<br><br>
+                Contact via email: <a class = "aboutLink" href="mailto:cozytypes@cozytypes.com">cozytypes@cozytypes.com</a>
                 </h1>
             </div>
         </div>
