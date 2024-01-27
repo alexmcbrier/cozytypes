4e9851db8a393d71e6c1f6f51675fa8873b5e312
Alexander McBrier <alexmcbrier@crc-dot1x-nat-10-239-247-139.bu.edu>
2024-01-27 11:29:51 -0500

    added stats

--- profile.php
+++ profile.php
@@ -176,48 +176,50 @@ if (isset($_SESSION["user_id"])) {
             <div id = "logo">personal bests</div>
             <div id="displayStats">
                 <div class="statsContainer">
-                    <div id = "preferenceHeader" class = "notSignedIn">15s</div>
+                    <div id = "preferenceHeader" class = "notSignedIn">15 seconds</div>
                     <div class = "results">wpm <span style = "color: var(--testText)"><?= $maxWpm15s ?></span></div>
                     <div class = "results">accuracy <span style = "color: var(--testText)"><?= $accuracyForMaxWpm15s ?></span></div>
                     <div class = "results">tests <span style = "color: var(--testText)"><?= $count15s ?></span></div>
                 </div>
                 <div class="statsContainer">
-                    <div id = "preferenceHeader" class = "notSignedIn">30s</div>
+                    <div id = "preferenceHeader" class = "notSignedIn">30 seconds</div>
                     <div class = "results">wpm <span style = "color: var(--testText)"><?= $maxWpm30s ?></span></div>
                     <div class = "results">accuracy <span style = "color: var(--testText)"><?= $accuracyForMaxWpm30s ?></span></div>
                     <div class = "results">tests <span style = "color: var(--testText)"><?= $count30s ?></span></div>
                 </div>
                 <div class="statsContainer">
-                    <div id = "preferenceHeader" class = "notSignedIn">60s</div>
+                    <div id = "preferenceHeader" class = "notSignedIn">60 seconds</div>
                     <div class = "results">wpm <span style = "color: var(--testText)"><?= $maxWpm60s ?></span></div>
                     <div class = "results">accuracy <span style = "color: var(--testText)"><?= $accuracyForMaxWpm60s ?></span></div>
                     <div class = "results">tests <span style = "color: var(--testText)"><?= $count60s ?></span></div>
                 </div>
-                <div class="statsContainer">
-                    <div id = "preferenceHeader" class = "notSignedIn">10w</div>
-                    <div class = "results">wpm <span style = "color: var(--testText)"><?= $maxWpm10w ?></span></div>
-                    <div class = "results">accuracy <span style = "color: var(--testText)"><?= $accuracyForMaxWpm10w ?></span></div>
-                    <div class = "results">tests <span style = "color: var(--testText)"><?= $count10w ?></span></div>
-                </div>
-                <div class="statsContainer">
-                    <div id = "preferenceHeader" class = "notSignedIn">25w</div>
-                    <div class = "results">wpm <span style = "color: var(--testText)"><?= $maxWpm25w ?></span></div>
-                    <div class = "results">accuracy <span style = "color: var(--testText)"><?= $accuracyForMaxWpm25w ?></span></div>
-                    <div class = "results">tests <span style = "color: var(--testText)"><?= $count25w ?></span></div>
-                </div>
-                <div class="statsContainer">
-                    <div id = "preferenceHeader" class = "notSignedIn">50w</div>
-                    <div class = "results">wpm <span style = "color: var(--testText)"><?= $maxWpm50w ?></span></div>
-                    <div class = "results">accuracy <span style = "color: var(--testText)"><?= $accuracyForMaxWpm50w ?></span></div>
-                    <div class = "results">tests <span style = "color: var(--testText)"><?= $count50w ?></span></div>
-                </div>
-                <div class="statsContainer">
-                    <div id = "preferenceHeader" class = "notSignedIn">100w</div>
-                    <div class = "results">wpm <span style = "color: var(--testText)"><?= $maxWpm100w ?></span></div>
-                    <div class = "results">accuracy <span style = "color: var(--testText)"><?= $accuracyForMaxWpm100w ?></span></div>
-                    <div class = "results">tests <span style = "color: var(--testText)"><?= $count100w ?></span></div>
-                </div>
             </div>
+            <div id="displayStats">
+                <div class="statsContainer">
+                        <div id = "preferenceHeader" class = "notSignedIn">10 words</div>
+                        <div class = "results">wpm <span style = "color: var(--testText)"><?= $maxWpm10w ?></span></div>
+                        <div class = "results">accuracy <span style = "color: var(--testText)"><?= $accuracyForMaxWpm10w ?></span></div>
+                        <div class = "results">tests <span style = "color: var(--testText)"><?= $count10w ?></span></div>
+                    </div>
+                    <div class="statsContainer">
+                        <div id = "preferenceHeader" class = "notSignedIn">25 words</div>
+                        <div class = "results">wpm <span style = "color: var(--testText)"><?= $maxWpm25w ?></span></div>
+                        <div class = "results">accuracy <span style = "color: var(--testText)"><?= $accuracyForMaxWpm25w ?></span></div>
+                        <div class = "results">tests <span style = "color: var(--testText)"><?= $count25w ?></span></div>
+                    </div>
+                    <div class="statsContainer">
+                        <div id = "preferenceHeader" class = "notSignedIn">50 words</div>
+                        <div class = "results">wpm <span style = "color: var(--testText)"><?= $maxWpm50w ?></span></div>
+                        <div class = "results">accuracy <span style = "color: var(--testText)"><?= $accuracyForMaxWpm50w ?></span></div>
+                        <div class = "results">tests <span style = "color: var(--testText)"><?= $count50w ?></span></div>
+                    </div>
+                    <div class="statsContainer">
+                        <div id = "preferenceHeader" class = "notSignedIn">100 words</div>
+                        <div class = "results">wpm <span style = "color: var(--testText)"><?= $maxWpm100w ?></span></div>
+                        <div class = "results">accuracy <span style = "color: var(--testText)"><?= $accuracyForMaxWpm100w ?></span></div>
+                        <div class = "results">tests <span style = "color: var(--testText)"><?= $count100w ?></span></div>
+                    </div>
+                </div>
             <a id = "showRestart" class="notSignedIn" href="logout.php">logout<i class="fa-solid fa-right-from-bracket"></i></a>
         </div>
         <?php include "./footer.php" ?>
