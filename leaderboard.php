60a8813bce95e10e084f2abde3b53ed39191fd8a
Alexander McBrier <alexmcbrier@crc-dot1x-nat-10-239-247-139.bu.edu>
2024-01-28 19:28:00 -0500

    fix

--- leaderboard.php
+++ leaderboard.php
@@ -11,99 +11,132 @@ $mysqli = require __DIR__ . "/config.php";
     <div id="mainContent">
         <?php include "./nav.php" ?>
         <div id="middle">
-        <div id = "showSignIn" style = "font-weight: bold; padding:0rem 2rem;" >Leaderboard<i class="fa-solid fa-medal"></i></div>
-        <div id = "displayStats">
-            <div class = "statsContainer">
-                <div id = "preferenceHeader" class = "notSignedIn">15 seconds </div>
-                <?php
-                $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND testTime = 15 ORDER BY wpm DESC LIMIT 5";
-                $result = $mysqli->query($query);
-                $rows = $result->fetch_all(MYSQLI_ASSOC);            
-                foreach ($rows as $row) {
-                    //now to get the username from the id
-                    $query = "SELECT * FROM user WHERE id = {$row['id']}";
+            <div id = "showSignIn" style = "font-weight: bold; padding:0rem 2rem;" >Leaderboard<i class="fa-solid fa-medal"></i></div>
+            <div id = "displayStats">
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">15 seconds </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND testTime = 15 ORDER BY wpm DESC LIMIT 5";
                     $result = $mysqli->query($query);
-                    $user = $result->fetch_assoc();
-                    echo '<div class = "results">' . $user['username'] . ' | ' . $row['wpm'] . ' WPM</div>';
-                    }
-                ?>
-            </div>
-            <div class = "statsContainer">
-                <div id = "preferenceHeader" class = "notSignedIn">  30 seconds  </div>
-                <?php
-                $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND testTime = 30 ORDER BY wpm DESC LIMIT 5";
-                $result = $mysqli->query($query);
-                $rows = $result->fetch_all(MYSQLI_ASSOC);            
-                foreach ($rows as $row) {
-                    //now to get the username from the id
-                    $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    foreach ($rows as $row) {
+                        //now to get the username from the id
+                        $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                        $result = $mysqli->query($query);
+                        $user = $result->fetch_assoc();
+                        echo '<div class = "results">' . $user['username'] . ' | ' . $row['wpm'] . ' WPM</div>';
+                        }
+                    ?>
+                </div>
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">  30 seconds  </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND testTime = 30 ORDER BY wpm DESC LIMIT 5";
                     $result = $mysqli->query($query);
-                    $user = $result->fetch_assoc();
-                    echo '<div class = "results">' . $user['username'] . ' | ' . $row['wpm'] . ' WPM</div>';
-                    }
-                ?>
-            </div>
-            <div class = "statsContainer">
-                <div id = "preferenceHeader" class = "notSignedIn">  60 seconds  </div>
-                <?php
-                $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND testTime = 60 ORDER BY wpm DESC LIMIT 5";
-                $result = $mysqli->query($query);
-                $rows = $result->fetch_all(MYSQLI_ASSOC);            
-                foreach ($rows as $row) {
-                    //now to get the username from the id
-                    $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    foreach ($rows as $row) {
+                        //now to get the username from the id
+                        $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                        $result = $mysqli->query($query);
+                        $user = $result->fetch_assoc();
+                        echo '<div class = "results">' . $user['username'] . ' | ' . $row['wpm'] . ' WPM</div>';
+                        }
+                    ?>
+                </div>
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">  60 seconds  </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND testTime = 60 ORDER BY wpm DESC LIMIT 5";
                     $result = $mysqli->query($query);
-                    $user = $result->fetch_assoc();
-                    echo '<div class = "results">' . $user['username'] . ' | ' . $row['wpm'] . ' WPM</div>';
-                    }
-                ?>
-            </div>
-            <div class = "statsContainer">
-                <div id = "preferenceHeader" class = "notSignedIn">15 seconds </div>
-                <?php
-                $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND testTime = 15 ORDER BY wpm DESC LIMIT 5";
-                $result = $mysqli->query($query);
-                $rows = $result->fetch_all(MYSQLI_ASSOC);            
-                foreach ($rows as $row) {
-                    //now to get the username from the id
-                    $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    foreach ($rows as $row) {
+                        //now to get the username from the id
+                        $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                        $result = $mysqli->query($query);
+                        $user = $result->fetch_assoc();
+                        echo '<div class = "results">' . $user['username'] . ' | ' . $row['wpm'] . ' WPM</div>';
+                        }
+                    ?>
+                </div>
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">  60 seconds  </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND testTime = 120 ORDER BY wpm DESC LIMIT 5";
                     $result = $mysqli->query($query);
-                    $user = $result->fetch_assoc();
-                    echo '<div class = "results">' . $user['username'] . ' | ' . $row['wpm'] . ' WPM</div>';
-                    }
-                ?>
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    foreach ($rows as $row) {
+                        //now to get the username from the id
+                        $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                        $result = $mysqli->query($query);
+                        $user = $result->fetch_assoc();
+                        echo '<div class = "results">' . $user['username'] . ' | ' . $row['wpm'] . ' WPM</div>';
+                        }
+                    ?>
+                </div>
             </div>
-            <div class = "statsContainer">
-                <div id = "preferenceHeader" class = "notSignedIn">15 seconds </div>
-                <?php
-                $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND testTime = 15 ORDER BY wpm DESC LIMIT 5";
-                $result = $mysqli->query($query);
-                $rows = $result->fetch_all(MYSQLI_ASSOC);            
-                foreach ($rows as $row) {
-                    //now to get the username from the id
-                    $query = "SELECT * FROM user WHERE id = {$row['id']}";
+
+            <div id = "displayStats">
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">15 seconds </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'words' AND testTime = 10 ORDER BY wpm DESC LIMIT 5";
                     $result = $mysqli->query($query);
-                    $user = $result->fetch_assoc();
-                    echo '<div class = "results">' . $user['username'] . ' | ' . $row['wpm'] . ' WPM</div>';
-                    }
-                ?>
-            </div>
-            <div class = "statsContainer">
-                <div id = "preferenceHeader" class = "notSignedIn">15 seconds </div>
-                <?php
-                $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND testTime = 15 ORDER BY wpm DESC LIMIT 5";
-                $result = $mysqli->query($query);
-                $rows = $result->fetch_all(MYSQLI_ASSOC);            
-                foreach ($rows as $row) {
-                    //now to get the username from the id
-                    $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    foreach ($rows as $row) {
+                        //now to get the username from the id
+                        $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                        $result = $mysqli->query($query);
+                        $user = $result->fetch_assoc();
+                        echo '<div class = "results">' . $user['username'] . ' | ' . $row['wpm'] . ' WPM</div>';
+                        }
+                    ?>
+                </div>
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">  30 seconds  </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'words' AND testTime = 25 ORDER BY wpm DESC LIMIT 5";
                     $result = $mysqli->query($query);
-                    $user = $result->fetch_assoc();
-                    echo '<div class = "results">' . $user['username'] . ' | ' . $row['wpm'] . ' WPM</div>';
-                    }
-                ?>
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    foreach ($rows as $row) {
+                        //now to get the username from the id
+                        $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                        $result = $mysqli->query($query);
+                        $user = $result->fetch_assoc();
+                        echo '<div class = "results">' . $user['username'] . ' | ' . $row['wpm'] . ' WPM</div>';
+                        }
+                    ?>
+                </div>
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">  60 seconds  </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'words' AND testTime = 50 ORDER BY wpm DESC LIMIT 5";
+                    $result = $mysqli->query($query);
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    foreach ($rows as $row) {
+                        //now to get the username from the id
+                        $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                        $result = $mysqli->query($query);
+                        $user = $result->fetch_assoc();
+                        echo '<div class = "results">' . $user['username'] . ' | ' . $row['wpm'] . ' WPM</div>';
+                        }
+                    ?>
+                </div>
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">  60 seconds  </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'words' AND testTime = 100 ORDER BY wpm DESC LIMIT 5";
+                    $result = $mysqli->query($query);
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    foreach ($rows as $row) {
+                        //now to get the username from the id
+                        $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                        $result = $mysqli->query($query);
+                        $user = $result->fetch_assoc();
+                        echo '<div class = "results">' . $user['username'] . ' | ' . $row['wpm'] . ' WPM</div>';
+                        }
+                    ?>
+                </div>
             </div>
-        </div>
         </div>
         <?php include "./footer.php" ?>
     </div>
