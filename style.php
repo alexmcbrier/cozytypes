34fccf42a897b261cc99bd9d09f37f07b4dbb8c6
Alexander McBrier <alexmcbrier@crc-dot1x-nat-10-239-247-139.bu.edu>
2024-01-30 15:21:00 -0500

    transition

--- style.php
+++ style.php
@@ -147,6 +147,7 @@ include "themes.scss"; //file contains all fonts
         width: 100%;
         margin: 0;
         font-family: var(--fontFamily);
+        transition: color .25s ease;
     }
     .main-body {
         overflow: hidden;
