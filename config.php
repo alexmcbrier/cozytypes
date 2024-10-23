0fc8ab3a3c5ef76739446bbe846d096684767f40
Alexander McBrier <58454148+alexmcbrier@users.noreply.github.com>
2024-10-23 16:20:12 -0400

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$host = "localhost";
-$dbname = "u885077784_cozytypes";
-$username = "u885077784_alexmcbrier";
-$password = "AMcB0807";
-
-$mysqli = new mysqli(hostname: $host, username: $username, password: $password, database: $dbname);
-return $mysqli;
