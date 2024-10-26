30a760617fc8516118ba7944bc5fa33512c336d0
alexmcbrier <alexmcbrier@icloud.com>
2024-10-26 14:12:12 -0400

    fix

--- booksScript.js
+++ booksScript.js
@@ -239,12 +239,34 @@ function restart() {
     hotkey.classList.remove('fadeOut');
     refresh();
 }
+function randomQuote() {
+    displayText.innerText = ''; // Remove previous sentence if applicable
+    let selectedTitle = getStorageItem("selectedTitle"); // Get the selected book/poem title
+    const text = texts[selectedTitle]; // Get the text based on selection
+ 
+ 
+ 
+    const words = text.split(/\s+/); // Split the text into words
+ 
+ 
+    for (let i = 0; i < words.length; i++) {
+        sentence[i] = words[i]; // Fill sentence with words from the text
+    }
+ }
+ 
  function randomQuote() {
    displayText.innerText = ''; // Remove previous sentence if applicable
 
 
    const selectedTitle = getStorageItem("selectedTitle"); // Get the selected book/poem title
    const text = texts[selectedTitle]; // Get the text based on selection
+
+
+   if (!text) {
+       //text not found can do something
+   }
+
+
    const words = text.split(/\s+/); // Split the text into words
 
 
