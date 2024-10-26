27bbc6da1497b8a97b2831a4ec56bbdd17bbbbf6
alexmcbrier <alexmcbrier@icloud.com>
2024-10-26 15:49:07 -0400

    d

--- booksScript.js
+++ booksScript.js
@@ -314,6 +314,7 @@ function startTimer() {
 
             // Calculate completion percentage based on letters
             const completionPercentage = (typedLetters / allLetters) * 100;
+            console.log(completionPercentage);
             displayCompletion.innerText = `Completion: ${Math.round(completionPercentage)}%`;
         }, 1000)
         timerStatus = true;
