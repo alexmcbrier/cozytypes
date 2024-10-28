1b7d46ed9dc6b324a41888c3abab7d58b6a29c0c
alexmcbrier <alexmcbrier@icloud.com>
2024-10-27 22:40:33 -0400

    update

--- leaderboard.php
+++ leaderboard.php
@@ -463,6 +463,230 @@ $mysqli = require __DIR__ . "/config.php";
                     ?>
                 </div>
             </div>
+
+
+            <div class = "profileValues" style = "padding: 0 1rem">This Week</div>
+            <div id = "displayStats">
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">15 seconds </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND testTime = 15 ORDER BY wpm DESC LIMIT 5";
+                    $result = $mysqli->query($query);
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    // Ensure there are at least 5 rows, adding empty rows if necessary
+                    while (count($rows) < 5) {
+                        $rows[] = ['id' => null, 'wpm' => null]; // Add an empty row
+                    }
+                    $count = 1;
+                    foreach ($rows as $row) {
+                        // Now to get the username from the id
+                        $username = 'xxx';
+                        if ($row['id'] !== null) {
+                            $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                            $result = $mysqli->query($query);
+                            $user = $result->fetch_assoc();
+                            $username = ($user !== null) ? $user['username'] : '---';
+                        }
+
+                        $wpm = ($row['wpm'] !== null) ? $row['wpm']: '---';
+                        echo '<div class="profileValues">' . $count . '. ' . $username . ' | ' . $wpm . '</div>';
+                        $count++;
+                    }
+                    ?>
+                </div>
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">30 seconds  </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND testTime = 30 ORDER BY wpm DESC LIMIT 5";
+                    $result = $mysqli->query($query);
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    // Ensure there are at least 5 rows, adding empty rows if necessary
+                    while (count($rows) < 5) {
+                        $rows[] = ['id' => null, 'wpm' => null]; // Add an empty row
+                    }
+                    $count = 1;
+                    foreach ($rows as $row) {
+                        // Now to get the username from the id
+                        $username = 'xxx';
+                        if ($row['id'] !== null) {
+                            $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                            $result = $mysqli->query($query);
+                            $user = $result->fetch_assoc();
+                            $username = ($user !== null) ? $user['username'] : '---';
+                        }
+
+                        $wpm = ($row['wpm'] !== null) ? $row['wpm']: '---';
+                        echo '<div class="profileValues">' . $count . '. ' . $username . ' | ' . $wpm . '</div>';
+                        $count++;
+                    }
+                    ?>
+                </div>
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">60 seconds  </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND testTime = 60 ORDER BY wpm DESC LIMIT 5";
+                    $result = $mysqli->query($query);
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    // Ensure there are at least 5 rows, adding empty rows if necessary
+                    while (count($rows) < 5) {
+                        $rows[] = ['id' => null, 'wpm' => null]; // Add an empty row
+                    }
+                    $count = 1;
+                    foreach ($rows as $row) {
+                        // Now to get the username from the id
+                        $username = 'xxx';
+                        if ($row['id'] !== null) {
+                            $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                            $result = $mysqli->query($query);
+                            $user = $result->fetch_assoc();
+                            $username = ($user !== null) ? $user['username'] : '---';
+                        }
+
+                        $wpm = ($row['wpm'] !== null) ? $row['wpm']: '---';
+                        echo '<div class="profileValues">' . $count . '. ' . $username . ' | ' . $wpm . '</div>';
+                        $count++;
+                    }
+                    ?>
+                </div>
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">120 seconds  </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND testTime = 120 ORDER BY wpm DESC LIMIT 5";
+                    $result = $mysqli->query($query);
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    // Ensure there are at least 5 rows, adding empty rows if necessary
+                    while (count($rows) < 5) {
+                        $rows[] = ['id' => null, 'wpm' => null]; // Add an empty row
+                    }
+                    $count = 1;
+                    foreach ($rows as $row) {
+                        // Now to get the username from the id
+                        $username = 'xxx';
+                        if ($row['id'] !== null) {
+                            $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                            $result = $mysqli->query($query);
+                            $user = $result->fetch_assoc();
+                            $username = ($user !== null) ? $user['username'] : '---';
+                        }
+
+                        $wpm = ($row['wpm'] !== null) ? $row['wpm']: '---';
+                        echo '<div class="profileValues">' . $count . '. ' . $username . ' | ' . $wpm . '</div>';
+                        $count++;
+                    }
+                    ?>
+                </div>
+            </div>
+
+            <div id = "displayStats">
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">10 words </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'words' AND testTime = 10 ORDER BY wpm DESC LIMIT 5";
+                    $result = $mysqli->query($query);
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    // Ensure there are at least 5 rows, adding empty rows if necessary
+                    while (count($rows) < 5) {
+                        $rows[] = ['id' => null, 'wpm' => null]; // Add an empty row
+                    }
+                    $count = 1;
+                    foreach ($rows as $row) {
+                        // Now to get the username from the id
+                        $username = 'xxx';
+                        if ($row['id'] !== null) {
+                            $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                            $result = $mysqli->query($query);
+                            $user = $result->fetch_assoc();
+                            $username = ($user !== null) ? $user['username'] : '---';
+                        }
+
+                        $wpm = ($row['wpm'] !== null) ? $row['wpm']: '---';
+                        echo '<div class="profileValues">' . $count . '. ' . $username . ' | ' . $wpm . '</div>';
+                        $count++;
+                    }
+                    ?>
+                </div>
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">25 words  </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'words' AND testTime = 25 ORDER BY wpm DESC LIMIT 5";
+                    $result = $mysqli->query($query);
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    // Ensure there are at least 5 rows, adding empty rows if necessary
+                    while (count($rows) < 5) {
+                        $rows[] = ['id' => null, 'wpm' => null]; // Add an empty row
+                    }
+                    $count = 1;
+                    foreach ($rows as $row) {
+                        // Now to get the username from the id
+                        $username = 'xxx';
+                        if ($row['id'] !== null) {
+                            $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                            $result = $mysqli->query($query);
+                            $user = $result->fetch_assoc();
+                            $username = ($user !== null) ? $user['username'] : '---';
+                        }
+
+                        $wpm = ($row['wpm'] !== null) ? $row['wpm']: '---';
+                        echo '<div class="profileValues">' . $count . '. ' . $username . ' | ' . $wpm . '</div>';
+                        $count++;
+                    }
+                    ?>
+                </div>
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">50 words  </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'words' AND testTime = 50 ORDER BY wpm DESC LIMIT 5";
+                    $result = $mysqli->query($query);
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    // Ensure there are at least 5 rows, adding empty rows if necessary
+                    while (count($rows) < 5) {
+                        $rows[] = ['id' => null, 'wpm' => null]; // Add an empty row
+                    }
+                    $count = 1;
+                    foreach ($rows as $row) {
+                        // Now to get the username from the id
+                        $username = 'xxx';
+                        if ($row['id'] !== null) {
+                            $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                            $result = $mysqli->query($query);
+                            $user = $result->fetch_assoc();
+                            $username = ($user !== null) ? $user['username'] : '---';
+                        }
+
+                        $wpm = ($row['wpm'] !== null) ? $row['wpm']: '---';
+                        echo '<div class="profileValues">' . $count . '. ' . $username . ' | ' . $wpm . '</div>';
+                        $count++;
+                    }
+                    ?>
+                </div>
+                <div class = "statsContainer">
+                    <div id = "preferenceHeader" class = "notSignedIn">100 words  </div>
+                    <?php
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'words' AND testTime = 100 ORDER BY wpm DESC LIMIT 5";
+                    $result = $mysqli->query($query);
+                    $rows = $result->fetch_all(MYSQLI_ASSOC);            
+                    // Ensure there are at least 5 rows, adding empty rows if necessary
+                    while (count($rows) < 5) {
+                        $rows[] = ['id' => null, 'wpm' => null]; // Add an empty row
+                    }
+                    $count = 1;
+                    foreach ($rows as $row) {
+                        // Now to get the username from the id
+                        $username = 'xxx';
+                        if ($row['id'] !== null) {
+                            $query = "SELECT * FROM user WHERE id = {$row['id']}";
+                            $result = $mysqli->query($query);
+                            $user = $result->fetch_assoc();
+                            $username = ($user !== null) ? $user['username'] : '---';
+                        }
+
+                        $wpm = ($row['wpm'] !== null) ? $row['wpm']: '---';
+                        echo '<div class="profileValues">' . $count . '. ' . $username . ' | ' . $wpm . '</div>';
+                        $count++;
+                    }
+                    ?>
+                </div>
+            </div>
         </div>
         <?php include "./footer.php" ?>
     </form>
