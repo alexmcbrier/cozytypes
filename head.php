6a79a832c424caa6fc7281eba54ee01fbc4df06c
alexmcbrier <alexmcbrier@icloud.com>
2024-12-01 00:31:47 -0500

    fix flash css bug on chrome

--- head.php
+++ head.php
@@ -31,6 +31,20 @@
             var clean_uri = uri.substring(0, uri.indexOf("?"));
             window.history.replaceState({}, document.title, clean_uri);
         }
+        //fix the flash before css loads
+        const theme = localStorage.getItem("theme") || "light";
+        document.documentElement.classList.add(theme);
+        const root = document.documentElement;
+        root.style.setProperty("--fontSize", localStorage.getItem("fontSize") || "3");
+        root.style.setProperty("--fontFamily", localStorage.getItem("fontFamily") || "lora");
+        root.style.setProperty("--lineCount", localStorage.getItem("lineCount") || "2");
+        root.style.setProperty("--caret", localStorage.getItem("caret") || "caret");
+        root.style.setProperty("--typingMode", localStorage.getItem("typingMode") || "time");
+        root.style.setProperty("--words", localStorage.getItem("words") || "10");
+        root.style.setProperty("--time", localStorage.getItem("time") || "15");
+        root.style.setProperty("--blur", localStorage.getItem("blur") || "off");
+        root.style.setProperty("--mode", localStorage.getItem("mode") || "easy");
+        root.style.setProperty("--selectedTitle", localStorage.getItem("selectedTitle") || "harryPotter");
     </script>
     <script src="https://kit.fontawesome.com/c76282caa4.js" crossorigin="anonymous"></script> 
     <meta name="google-adsense-account" content="ca-pub-9923722164132738">
