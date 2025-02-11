db11dc59dfb7ddfc494786e78c63013dd54d5fe4
Alex McBrier <alexmcbrier@icloud.com>
2025-02-10 23:42:16 -0500

    fix

--- index.php
+++ index.php
@@ -162,7 +162,7 @@ crossorigin="anonymous"></script>
                 </h1>
                 
                 <div style = "font-size: 2rem; color: var(--row); padding: 0rem 2rem;">
-                    Upgrade your setup with MelGeek. <span style = "color: var(--currentword)">8% off</span>Use code COZYTYPES for  keyboards, switches, and keycaps.
+                    Upgrade your setup with MelGeek. <span style = "color: var(--currentword)">Use code COZYTYPES for 8% off</span> keyboards, switches, and keycaps.
                 </div>
                 <a id = "shopnow" class="preference" style = "width: 90%" href="https://www.melgeek.com/?ref=cozytypes">Shop Now</a>
             </div>
