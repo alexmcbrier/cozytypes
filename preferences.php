e819d79a558b28e9036caae6ab0009c7414adaed
Alex McBrier <alexmcbrier@icloud.com>
2025-02-27 14:47:59 -0500

    change form head 1 to 2

--- preferences.php
+++ preferences.php
@@ -4,7 +4,7 @@ session_start();
 <!DOCTYPE html>
 <html>
 
-<?php include "./head2.php" ?>
+<?php include "./head.php" ?>
 <head>
 <title>preferences | cozytypes</title>
 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9923722164132738"
