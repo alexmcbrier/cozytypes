64d7367263aa51cc193c80bbceb079835ab9a20b
Alex McBrier <alexmcbrier@icloud.com>
2025-02-27 14:56:37 -0500

    have hotkey only if index.php

--- footer.php
+++ footer.php
@@ -6,12 +6,15 @@ if (isset($_COOKIE["theme"])) {
 if (isset($_COOKIE["fontFamily"])) {
     $font = $_COOKIE["fontFamily"];
 }
+$isHomePage = basename($_SERVER['PHP_SELF']) == 'index.php';
 ?>
 
 <div id="footer" class = "invisible">
-    <h2 id ="hotkey">
-        <div><kbd>Tab</kbd> to restart</div>
-    </h2>
+    <?php if ($isHomePage): ?>
+        <h2 id="hotkey">
+            <div><kbd>Tab</kbd> to restart</div>
+        </h2>
+    <?php endif; ?>
     <div id = "footerDiv">
         <!-- 
         <a class = "footerLinks tooltip" href="https://github.com/alexmcbrier/cozytypes">github</span>
