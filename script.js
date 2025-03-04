f509d17682dc250d6b6f85e8fdf1dcbd511242be
Alex McBrier <alexmcbrier@icloud.com>
2025-03-04 18:21:38 -0500

    do 3

--- script.js
+++ script.js
@@ -204,7 +204,7 @@ function keystroke() {
     }
 }
 function switchClick(switchtype) { // a function that when given a switch type say novel key cream, produces random sound of nk cream clicking.
-    switches = ["sounds/switches/" + switchtype + "/click1.aac", "sounds/switches/" + switchtype + "/click2.aac", "", "sounds/switches/" + switchtype + "/click3.aac", "", "sounds/switches/" + switchtype + "/click4.aac", "", "sounds/switches/" + switchtype + "/click5.aac", "", "sounds/switches/" + switchtype + "/click6.aac", ""];
+    switches = ["sounds/switches/" + switchtype + "/click1.aac", "sounds/switches/" + switchtype + "/click2.aac", "sounds/switches/" + switchtype + "/click3.aac"];
     let randomIndex = Math.floor(Math.random() * switches.length);
     let randomFile =  switches[randomIndex];
     let audio = new Audio(randomFile);
