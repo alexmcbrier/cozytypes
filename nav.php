07e4036de0fe1fac199251a6b4ddf7bfdb5cdab8
Alex McBrier <alexmcbrier@icloud.com>
2025-03-04 21:25:11 -0500

    make open in new tabg

--- nav.php
+++ nav.php
@@ -13,7 +13,7 @@ if (isset($_SESSION["user_id"])) {
     <a class = "navIcon" href="/preferences"><i class="fa-solid fa-gear"></i></a>
     <a class = "navIcon" href="/leaderboard"><i class="fa-solid fa-crown"></i></a>
     <a class = "navIcon" href="/about"><i class="fa-solid fa-info"></i></a>
-    <a class = "navIcon" href="https://imissmycafe.com"><i class="fa-solid fa-mug-hot"></i></a>
+    <a class = "navIcon" href="https://imissmycafe.com" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-mug-hot"></i></a>
     <a href="/signup" id = "showUsername">
         <i class="fa-regular fa-user"></i>
         <div><?= htmlspecialchars($user["username"]) ?></div>
