0b2cceb6f32b13550348385c6ad8cd5c40558a99
Alex McBrier <alexmcbrier@icloud.com>
2025-03-28 15:40:55 -0400

    change to preference like

--- catalog.php
+++ catalog.php
@@ -14,29 +14,63 @@ session_start();
     <form id="mainContent" method="POST" action="preferences.php">
         <?php include "./nav.php" ?>
         <div id = "showSignIn" style = "padding:0rem 2rem;" >Catalog<i class="fa-solid fa-crown"></i></div>
-        <div class = "profileValues" style = "padding: 0 1rem">must have an account to be on the leaderboard</div>
-        <div>
-            <div class="statsContainer" style = "background-color: var(--rowBackground);">
-                <h1 class="notSignedIn" id="preferenceHeader">keyboards<i class="fa-solid fa-keyboard"></i></h1>
-                <a class="results">hello</a>
+        <div class = "profileValues" style = "padding: 0 1rem">some links help support the site, but all picks are based on what the typing community reccomends.</div>
+        
+        <div class="preferences">
+            <div id="themesContainer" class="theme-row-container">
+                <div>
+                    <h1 id="preferenceHeader">keyboards<i class="fa-solid fa-keyboard"></i></h1>
+                </div>
+                <div class="theme-row-container">
+                    <a class = "color-theme click light " onclick="setTheme(currentTheme, 'light')">light</a>
+                    <a class = "color-theme click theme-9009" onclick="setTheme(currentTheme, 'theme-9009')">9009</a>
+                    <a class = "color-theme click godspeed" onclick="setTheme(currentTheme, 'godspeed')">godspeed</a>
+                </div>
             </div>
-            <div class="statsContainer" style = "background-color: var(--rowBackground);">
-                <h1 class="notSignedIn" id="preferenceHeader">keycaps<i class="fa-solid fa-square-h"></i></h1>
-                <a class="results">hello</a>
+            <div id="themesContainer" class="theme-row-container">
+                <div>
+                    <h1 id="preferenceHeader">keycaps<i class="fa-solid fa-square-h"></i></h1>
+                </div>
+                <div class="theme-row-container">
+                    <a class = "color-theme click light " onclick="setTheme(currentTheme, 'light')">light</a>
+                    <a class = "color-theme click theme-9009" onclick="setTheme(currentTheme, 'theme-9009')">9009</a>
+                    <a class = "color-theme click godspeed" onclick="setTheme(currentTheme, 'godspeed')">godspeed</a>
+                </div>
             </div>
-            <div class="statsContainer" style = "background-color: var(--rowBackground);">
-                <h1 class="notSignedIn" id="preferenceHeader">switches<i class="fa-solid fa-inbox"></i></h1>
-                <a class="results">hello</a>
+        </div>
+        <div id="themesContainer" class="theme-row-container">
+                <div>
+                    <h1 id="preferenceHeader">switches<i class="fa-solid fa-inbox"></i></h1>
+                </div>
+                <div class="theme-row-container">
+                    <a class = "color-theme click light " onclick="setTheme(currentTheme, 'light')">light</a>
+                    <a class = "color-theme click theme-9009" onclick="setTheme(currentTheme, 'theme-9009')">9009</a>
+                    <a class = "color-theme click godspeed" onclick="setTheme(currentTheme, 'godspeed')">godspeed</a>
+                </div>
             </div>
-            <div class="statsContainer" style = "background-color: var(--rowBackground);">
-                <h1 class="notSignedIn" id="preferenceHeader">accesories<i class="fa-solid fa-wand-magic-sparkles"></i></h1>
-                <a class="results">hello</a>
+        </div>
+        <div id="themesContainer" class="theme-row-container">
+                <div>
+                    <h1 id="preferenceHeader">accesories<i class="fa-solid fa-wand-magic-sparkles"></i></h1>
+                </div>
+                <div class="theme-row-container">
+                    <a class = "color-theme click light " onclick="setTheme(currentTheme, 'light')">light</a>
+                    <a class = "color-theme click theme-9009" onclick="setTheme(currentTheme, 'theme-9009')">9009</a>
+                    <a class = "color-theme click godspeed" onclick="setTheme(currentTheme, 'godspeed')">godspeed</a>
+                </div>
             </div>
-            <div class="statsContainer" style = "background-color: var(--rowBackground);">
-                <h1 class="notSignedIn" id="preferenceHeader">diy & customization<i class="fa-solid fa-pen"></i></h1>
-                <a class="results">hello</a>
+        </div>
+        <div id="themesContainer" class="theme-row-container">
+                <div>
+                    <h1 id="preferenceHeader">diy & customization<i class="fa-solid fa-pen"></i></h1>
+                </div>
+                <div class="theme-row-container">
+                    <a class = "color-theme click light " onclick="setTheme(currentTheme, 'light')">light</a>
+                    <a class = "color-theme click theme-9009" onclick="setTheme(currentTheme, 'theme-9009')">9009</a>
+                    <a class = "color-theme click godspeed" onclick="setTheme(currentTheme, 'godspeed')">godspeed</a>
+                </div>
             </div>
-        </div> 
+        </div>
         <?php include "./footer.php" ?>
     </form>
 </body>
