e3da2b28cd1869090403d1dc111b82b5cf5d7ecc
Alex McBrier <alexmcbrier@icloud.com>
2025-03-28 13:32:47 -0400

    make vertical

--- catalog.php
+++ catalog.php
@@ -13,31 +13,7 @@ session_start();
 <body>
     <form id="mainContent" method="POST" action="preferences.php">
         <?php include "./nav.php" ?>
-
-
-        <div id="themesContainer" class="theme-row-container">
-                <div class="theme-row-container">
-                    <a class = "color-theme click light " onclick="setTheme(currentTheme, 'light')">light</a>
-                    <a class = "color-theme click theme-9009" onclick="setTheme(currentTheme, 'theme-9009')">9009</a>
-                    <a class = "color-theme click godspeed" onclick="setTheme(currentTheme, 'godspeed')">godspeed</a>
-                    <a class = "color-theme click blueberryLight" onclick="setTheme(currentTheme, 'blueberryLight')">blueberry light</a>
-                    <a class = "color-theme click mizu" onclick="setTheme(currentTheme, 'mizu')">mizu</a>
-                    <a class = "color-theme click botanical" onclick="setTheme(currentTheme, 'botanical')">botanical</a>
-                    <a class = "color-theme click darling" onclick="setTheme(currentTheme, 'darling')">darling</a>
-                    <a class = "color-theme click amethyst" onclick="setTheme(currentTheme, 'amethyst')">amethyst</a>
-                    <a class = "color-theme click strawberry" onclick="setTheme(currentTheme, 'strawberry')">strawberry</a>
-                    <a class = "color-theme click striker" onclick="setTheme(currentTheme, 'striker')">striker</a>
-                    <a class = "color-theme click alpine" onclick="setTheme(currentTheme, 'alpine')">alpine</a>
-                    <a class = "color-theme click theme-8008" onclick="setTheme(currentTheme, 'theme-8008')">8008</a>
-                    <a class = "color-theme click blueberry" onclick="setTheme(currentTheme, 'blueberry')">blueberry dark</a>
-                    <a class = "color-theme click nord" onclick="setTheme(currentTheme, 'nord')">nord</a>
-                    <a class = "color-theme click bliss" onclick="setTheme(currentTheme, 'bliss')">bliss</a>
-                    <a class = "color-theme click olivia" onclick="setTheme(currentTheme, 'olivia')">olivia</a>
-                    <a class = "color-theme click dracula" onclick="setTheme(currentTheme, 'dracula')">dracula</a>
-                    <a class = "color-theme click dark" onclick="setTheme(currentTheme, 'dark')">dark</a>
-                </div>
-            </div>
-        <div id="displayStats" style = "background-color: var(--background); margin: 1rem 0rem">
+        <div>
             <div class="statsContainer" style = "background-color: var(--rowBackground);">
                 <h1 class="notSignedIn" id="preferenceHeader">keyboards<i class="fa-solid fa-keyboard"></i></h1>
                 <a class="results">hello</a>
@@ -52,7 +28,7 @@ session_start();
             </div>
             
         </div>
-        <div id="displayStats" style = "background-color: var(--background); margin: 1rem 0rem">
+        <div>
             <div class="statsContainer" style = "background-color: var(--rowBackground);">
                 <h1 class="notSignedIn" id="preferenceHeader">accesories<i class="fa-solid fa-wand-magic-sparkles"></i></h1>
                 <a class="results">hello</a>
