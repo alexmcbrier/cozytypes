da7ba3aa22f247be079615ef77822ae25d76ad0c
Alex McBrier <alexmcbrier@icloud.com>
2025-04-03 14:46:28 -0400

    revert

--- script.js
+++ script.js
@@ -45,7 +45,6 @@ document.addEventListener("DOMContentLoaded", function () {
                         newContent.style.opacity = "0"; // Start hidden
                         newContent.style.transition = "opacity 0.3s ease-in";
                         newContent.style.opacity = "1"; // Fade in new content
-                        newContent.style.transition = "none";
                         loadPreferences();
                         if (window.location.pathname === "/preferences" || window.location.pathname === "/about" || window.location.pathname === "/leaderoard") {
                             // Apply styles to allow scrolling on preferences page
@@ -61,6 +60,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     }, 300); // Wait for fade-out before replacing
                 
                     history.pushState(null, "", url); // Update URL
+                    newContent.style.transition = "none";
                 }
             })
             .catch(error => console.error("Page load failed:", error));
