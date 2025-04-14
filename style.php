bc8f712ac1d801f14c7dc24be384468f9a3fd682
Alex McBrier <alexmcbrier@icloud.com>
2025-04-14 00:55:21 -0400

    d

--- style.php
+++ style.php
@@ -583,7 +583,7 @@ include "themes.scss"; //file contains all fonts
         transform: translate(-50%, -50%);
         width: 250px;
         height: 250px;
-        transition: opacity .75s ease;
+        transition: opacity 1s ease;
         fill: var(--currentWord);
         }
     @keyframes fillBar {
@@ -591,7 +591,7 @@ include "themes.scss"; //file contains all fonts
         transform: scaleX(0);
     }
     to {
-        transform: scaleX(1.25);
+        transform: scaleX(1.5);
     }
     }
 
