bb07fa3e818f3d20799d8ddc39a5f34116544a29
Alex McBrier <alexmcbrier@icloud.com>
2025-04-14 01:12:12 -0400

    add

--- index.php
+++ index.php
@@ -69,14 +69,7 @@ crossorigin="anonymous"></script>
 <body class="main-body">
     <?php if (!isset($_GET["finish"])) { ?> <!-- only show if taking test, not complete -->
         <div id="cursor"></div>
-    <?php } ?>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JMV592" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-    <?php if (!isset($_GET["finish"])) { ?> <!-- only show if taking test, not complete -->
-        <div id="cursor"></div>
-    <?php } ?>
-    <?xml version="1.0" encoding="UTF-8"?>
+        <?xml version="1.0" encoding="UTF-8"?>
     <svg id="loadingBar" data-name="Layer 3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 556.54 171.88">
     <g>
         <g>
@@ -236,6 +229,10 @@ crossorigin="anonymous"></script>
         </g>
     </g>
     </svg>
+    <?php } ?>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5JMV592" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <div id="mainContent">
         <?php include "./nav.php" ?>
         <script type="text/javascript">
