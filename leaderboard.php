e661f9f916ef3d5c7316666fd37775d81b06bd3a
Alex McBrier <alexmcbrier@icloud.com>
2025-04-23 16:33:54 -0400

    revert

--- leaderboard.php
+++ leaderboard.php
@@ -133,18 +133,7 @@ $mysqli = require __DIR__ . "/config.php";
                 <div class = "statsContainer" style = "padding: 0; margin: 0">
                     <div id = "leaderboardheader" class = "notSignedIn">10 words </div>
                     <?php
-                    $query = "SELECT *
-                    FROM (
-                        SELECT *, ROW_NUMBER() OVER (PARTITION BY id ORDER BY wpm DESC) AS rn
-                        FROM typingtest
-                        WHERE mode = 'words' AND testTime = 10 AND wpm < 250
-                    ) AS ranked
-                    WHERE rn = 1
-                    ORDER BY wpm DESC
-                    LIMIT 5
-                ";
-                
-                
+                    $query = "SELECT * FROM typingtest WHERE id IS NOT NULL AND mode = 'words' AND wpm < 250 AND testTime = 10 ORDER BY wpm DESC LIMIT 5";
                     $result = $mysqli->query($query);
                     $rows = $result->fetch_all(MYSQLI_ASSOC);            
                     // Ensure there are at least 5 rows, adding empty rows if necessary
