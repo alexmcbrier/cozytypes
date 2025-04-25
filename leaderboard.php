d249904d867d9add6673c2b97b507dc66b00c2f3
Alex McBrier <alexmcbrier@icloud.com>
2025-04-25 00:42:55 -0400

    fixed issue now id ascending

--- leaderboard.php
+++ leaderboard.php
@@ -22,7 +22,7 @@ $mysqli = require __DIR__ . "/config.php";
                 <div class = "statsContainer" style = "padding: 0">
                     <div id = "leaderboardheader" class = "notSignedIn">15 seconds </div>
                     <?php
-                    $query = "SELECT id, MAX(wpm) AS best_wpm FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND wpm < 250 AND testTime = 15 GROUP BY id ORDER BY best_wpm DESC LIMIT 5;";
+                    $query = "SELECT id, MAX(wpm) AS best_wpm FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND wpm < 250 AND testTime = 15 GROUP BY id ORDER BY best_wpm DESC, id ASC LIMIT 5;";
                     $result = $mysqli->query($query);
                     $rows = $result->fetch_all(MYSQLI_ASSOC);            
                     // Ensure there are at least 5 rows, adding empty rows if necessary
@@ -49,7 +49,7 @@ $mysqli = require __DIR__ . "/config.php";
                 <div class = "statsContainer" style = "padding: 0">
                     <div id = "leaderboardheader" class = "notSignedIn">30 seconds  </div>
                     <?php
-                    $query = "SELECT id, MAX(wpm) AS best_wpm FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND wpm < 250 AND testTime = 30 GROUP BY id ORDER BY best_wpm DESC LIMIT 5;";
+                    $query = "SELECT id, MAX(wpm) AS best_wpm FROM typingtest WHERE id IS NOT NULL AND mode = 'time' AND wpm < 250 AND testTime = 30 GROUP BY id ORDER BY best_wpm DESC, id ASC LIMIT 5;";
                     $result = $mysqli->query($query);
                     $rows = $result->fetch_all(MYSQLI_ASSOC);            
                     // Ensure there are at least 5 rows, adding empty rows if necessary
